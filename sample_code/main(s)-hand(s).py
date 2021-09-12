584ad07f9147f141c132fdc5620df836a57d9985
Jikan <jikandev@gmail.com>
2021-09-12 18:15:46 +0200

    Update main(s)-hand(s).py

--- sample_code/main(s)-hand(s).py
+++ sample_code/main(s)-hand(s).py
@@ -2,11 +2,11 @@ import jikanvision
 import cv2
 
 cap = cv2.VideoCapture(0)
-handDetector = jikanvision.HandDetector(maxHands=2)
+handDetector = jikanvision.HandDetector()
 
 while True:
     success, img = cap.read()
-    img = handDetector.findHands(img)
+    hands, img = handDetector.findHands(img)
 
     cv2.imshow("Jikan Vision Library - HandDetector Module", img)
     cv2.waitKey(1)
